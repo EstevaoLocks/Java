8744b8efe810e0424dd31ec4ebc2703984339139
estevaolocks <estevao.locks11@gmail.com>
2025-10-28 11:44:33 -0300

    sincronizar

--- curso_SENAI/agechecker/AgeChecker.java
+++ curso_SENAI/agechecker/AgeChecker.java
@@ -10,6 +10,7 @@ import java.util.Scanner;
  * Criacao da classe AgeCecker para entender
  * o uso de inputs com operadores condicionais
  */
+
 public class AgeChecker {
 	public static void main(String[] args) {
 
